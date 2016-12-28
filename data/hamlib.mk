30f3c374a014c0e3fd62205f8b778cb6031d777e
Greg Beam <ki7mt@yahoo.com>
2016-12-28 02:08:24 -0700

    Update hamlib.mk and build functions

--- data/hamlib.mk
+++ data/hamlib.mk
@@ -1,26 +1,58 @@
-# This file is part of MXE.
-# See index.html for further information.
+# This file is part of MXE. See LICENSE.md for licensing information.
+# visit http://hamlib.org or https://github.com/N0NB/hamlib
+# 2016-12-24 Lars Holger Engelhard DL5RCW
 
 PKG             := hamlib
+$(PKG)_WEBSITE  := http://www.hamlib.org
+$(PKG)_DESCR    := HamLib 
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2~git
-$(PKG)_CHECKSUM := c3094b631ee88cd5147ba1b4bb9600343b17e7cb4b730a6d90e9dc36205a5e5d
+$(PKG)_VERSION  := 3.0.1
+$(PKG)_CHECKSUM := 3fec97ea326d02aa8f35834c4af34194a3f544e6212f391397d788c566b44e32
 $(PKG)_SUBDIR   := hamlib-$($(PKG)_VERSION)
-$(PKG)_FILE     := hamlib-$($(PKG)_VERSION)-3fef67a-20161226.tar.gz
-$(PKG)_URL      := http://n0nb.users.sourceforge.net/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc libxml2 libtool libltdl
+$(PKG)_FILE     := Hamlib-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/hamlib/hamlib/$($(PKG)_VERSION)/hamlib-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL_2    := https://github.com/N0NB/$(PKG)/archive/$($(PKG)_VERSION).tar.gz
 
+$(PKG)_DEPS     := gcc libxml2 libtool libltdl gcc pthreads libusb1
+
+# grabbing version from sourceforge
+# prefered by Nate N0NB
 define $(PKG)_UPDATE
-    echo 'TODO: write update script for $(PKG).' >&2;
-    echo $($(PKG)_VERSION)
+    $(WGET) -q -O- 'http://sourceforge.net/projects/hamlib/files/hamlib/' | \
+    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+    head -1
 endef
+
+# grabbing version from github
+#define $(PKG)_UPDATE
+#    $(WGET) -q -O- 'https://github.com/N0NB/$(PKG)/tags' | \
+#    $(SED) -n 's,.*href="/N0NB/$(PKG)/archive/\([0-9][^"]*\)\.tar.*,\1,p' | \
+#    head -1
+#endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure \
+pwd
+ cd '$(1)' && pwd && ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        --disable-winradio \
-        --without-cxx-binding \
-        --enable-static \
-        --disable-shared
+	--prefix='$(PREFIX)/$(TARGET)' \
+	--with-included-ltdl \
+        --disable-winradio 
     $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+
+    # create pkg-config files
+    $(INSTALL) -d '$(PREFIX)/$(TARGET)/lib/pkgconfig'
+    (echo 'Name: $(PKG)'; \
+     echo 'Version: $(hamlib_VERSION)'; \
+     echo 'Description: $(PKG)'; \
+     echo 'Cflags: -DHAMLIB_LIB'; \
+     echo 'Libs: -lhamlib -lpthread';) \
+     > '$(PREFIX)/$(TARGET)/lib/pkgconfig/$(PKG).pc'
+
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(PWD)/src/$(PKG)-test.c' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
+        `'$(TARGET)-pkg-config' hamlib --cflags --libs`
+
+
 endef
+
